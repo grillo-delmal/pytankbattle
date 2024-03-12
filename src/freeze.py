ce43d60982e612b4e18aee8ff397693113be4b60
Grillo del Mal <grillo@delmal.cl>
2024-03-12 02:07:14 -0300

    Fix licensing

--- src/freeze.py
+++ src/freeze.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2023, Grillo del Mal
+# 
+# Distributed under the 2-Clause BSD License, see LICENSE file.
+
 from cx_Freeze import setup, Executable
 import setuptools_git_versioning
 
--- src/pytankbattle.py
+++ src/pytankbattle.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2023, Grillo del Mal
+# 
+# Distributed under the 2-Clause BSD License, see LICENSE file.
+
 from pytankbattle import main
 
 main()
